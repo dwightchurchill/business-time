b98060a9824193eac225ccd01cfadff7fc5359bc
dwightchurchill <dwight.a.churchill@gmail.com>
2015-11-10 17:35:52 -0500

    Added slack stuff

--- bathroom-slack.rb
+++ bathroom-slack.rb
@@ -1,17 +1,33 @@
 require 'sinatra'
 require 'httparty'
 
+InvalidTokenError = Class.new(Exception)
+
 post '/' do
 
-  response = HTTParty.get('http://businesstime.localytics.com/api')
+  raise(InvalidTokenError) unless params[:token] == '2BEFUpP1nHFKvh20EHT8r9Cb'
 
-  men_left_stall_status = response[0]["value"]
-  men_right_stall_status = response[1]["value"]
-  women_left_stall_status = response[2]["value"]
-  women_right_stall_status = response[3]["value"]
+  text = params.fetch("text").strip
 
-  <<-TEXT
-  The left stall is #{men_left_stall_status} and the right stall is #{men_right_stall_status}. 
-  TEXT
+  if text == 'mens'
+
+    response = HTTParty.get('http://businesstime.localytics.com/api')
+
+    men_left_stall_status = response[0]["value"]
+    men_right_stall_status = response[1]["value"]
+    women_left_stall_status = response[2]["value"]
+    women_right_stall_status = response[3]["value"]
+
+    <<-TEXT
+    The left stall is #{men_left_stall_status} and the right stall is #{men_right_stall_status}. 
+    TEXT
+
+  else
+
+    <<-TEXT
+    We only surface information on the men's room currently!
+    TEXT
+    
+  end 
 
 end
